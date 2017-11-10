288cc1b39bd2f2f6c0e3ad406d21621acfc63407
uvblu <uvblu@LAPTOP-KOKCMQF8>
2017-11-10 21:59:34 +1100

    fghgh

---
+++ Assets/Script/LocalUser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(FPS))]
+public class LocalUser : MonoBehaviour
+{
+    private FPS player;
+	void Start ()
+    {
+        player = GetComponent<FPS>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        player.MoveCam();
+	}
+}
---
+++ Assets/Script/Network.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(FPS))]
+
+public class Network : NetworkBehaviour
+{
+    public Camera cam;
+    public AudioListener al;
+
+    [SyncVar] Vector3 syncPos;
+    [SyncVar] Quaternion syncRot;
+    [Range(0, 1)]
+    public float lerpRate = 1;
+
+    private FPS player;
+
+    private void Start()
+    {
+        player = GetComponent<FPS>();
+        
+        if(!isLocalPlayer)
+        {
+            al.enabled = false;
+            cam.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            player.MoveCam();
+            CharacterController controller = player.controller;
+            Cmd_SendPositionToServer(controller.transform.position);
+            Cmd_SendRotationToServer(controller.transform.rotation);
+        }
+        else
+        {
+            LerpPos();
+            LerpRot();
+        }
+	}
+
+    [Command]
+    void Cmd_SendPositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    [Command]
+    void Cmd_SendRotationToServer(Quaternion rot)
+    {
+        syncRot = rot;
+    }
+
+    void LerpPos()
+    {
+        CharacterController controller = player.controller;
+        controller.transform.position = Vector3.Lerp(controller.transform.position, syncPos, lerpRate);
+    }
+
+    void LerpRot()
+    {
+        CharacterController controller = player.controller;
+        controller.transform.rotation = Quaternion.Lerp(controller.transform.rotation, syncRot, lerpRate);
+    }
+}
